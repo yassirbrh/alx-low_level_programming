8b223736e4e2cb6bbaa63372e655e7b92e536b18
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-03-17 15:15:44 -0700

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main- Entry point
+ *
+ * Description: Prints the exact string with new line to the standard error.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return 1;
+}
