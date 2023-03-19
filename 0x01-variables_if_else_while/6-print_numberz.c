6c916b7798ecf0b82297f475003dfb01c49605c3
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-03-18 17:16:46 -0700

    prints all single numbers of base 10 with character

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base 10 using characters
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar((char)i);
+	}
+	putchar('\n');
+	return (0);
+}
