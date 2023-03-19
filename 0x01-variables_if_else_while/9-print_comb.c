cd5bc5f6ab2075ee5696e7782847079860a0d877
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-03-18 17:46:42 -0700

    prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Description: prints all possible combinations of single-digit numbers.
+ * Description: prints all possible combinations of single-digit numbers
  *
  * Return: Always 0 (Success)
 */
