4a3c64a1e7ce04bdce99356bfb730b0923846d77
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-03-20 12:59:24 -0700

    C program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,16 +8,26 @@
 */
 int main(void)
 {
-	int i, j, k, l;
+	int i, j, k, l, m;
 
 	for (i = 48; i < 58; i++)
 	{
 		for (j = 48; j < 58; j++)
 		{
+			if (i == 57 && j == 57)
+			{
+				break;
+			}
+			m = 0;
 			for (k = i; k < 58; k++)
 			{
-				for (l = j + 1; l < 58; l++)
+				for (l = 48; l < 58; l++)
 				{
+					if (m == 0)
+					{
+						l = j + 1;
+						m = 1;
+					}
 					if (i != k || j != l)
 					{
 					if ((i != 57 || j != 57 || k != 57 || l != 57) && (i != 48 || j != 48 || k != 48 || l != 49))
