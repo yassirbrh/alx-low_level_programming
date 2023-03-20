847e996795e46227443ce6b02e47df69e58b511d
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-03-20 16:41:02 -0700

    prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,42 +8,23 @@
 */
 int main(void)
 {
-	int i, j, k, l, m;
+	int i, j;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i < 99; i++)
 	{
-		for (j = 48; j < 58; j++)
+		for (j = i + 1; j < 100; j++)
 		{
-			if (i == 57 && j == 57)
+			if (i != j)
 			{
-				break;
-			}
-			m = 0;
-			for (k = i; k < 58; k++)
-			{
-				for (l = 48; l < 58; l++)
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
-					if (m == 0)
-					{
-						l = j + 1;
-						m = 1;
-					}
-					if (i != k || j != l)
-					{
-					if (i != 57 || j != 57 || k != 57 || l != 57)
-					{
-					if (i != 48 || j != 48 || k != 48 || l != 49)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					}
-					putchar((char)i);
-					putchar((char)j);
+					putchar(',');
 					putchar(' ');
-					putchar((char)k);
-					putchar((char)l);
-					}
 				}
 			}
 		}
