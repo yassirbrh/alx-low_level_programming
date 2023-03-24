89c65096d6cd664282ddc985c88f5fd761977595
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-03-24 08:30:58 -0700

    function that draws a diagonal line on the terminal.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - Function
+ *
+ * Description: Prints a square
+ *
+ * @size: the size of square
+ * 
+ * Return: No return because it's a void function
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size < 1)
+	_putchar('\n');
+}
