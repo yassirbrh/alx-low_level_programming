38e371d48e28d9ec5b29d01335e7282264af0c0a
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-03-24 11:33:30 -0700

    Fizz Buzz from 0 to 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,14 +17,16 @@ int main(void)
 		if (i % 3 == 0)
 		{
 			if (i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 			else
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
-		printf("Buzz ");
+		printf("Buzz");
 		else
-		printf("%d ", i);
+		printf("%d", i);
+		if (i < 100)
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
