7a8d9db9a0e0e1342fff08999e8b277bd5e4d385
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-03-30 03:21:29 -0700

    Function that copies a string within just n bytes from the source.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - Function
+ *
+ * Description: Copies a string within n bytes from src
+ *
+ * @dest: Pointer to the result string
+ * @src: Pointer to the source string
+ * @n: number of bytes to copy from string
+ *
+ * Return: Pointer to the result string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
