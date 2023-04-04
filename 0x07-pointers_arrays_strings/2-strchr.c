09cd97aae604749703a3ae4928314c1f4a0b6cd2
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-04-03 23:46:22 -0700

    Locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,13 +15,13 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	if (c == '\0')
-	return (NULL);
 	while (*(s + i) != '\0')
 	{
 		if (s[i] == c)
 		return (s + i);
 		i++;
 	}
+	if (c == '\0')
+	return (s + i);
 	return (NULL);
 }
