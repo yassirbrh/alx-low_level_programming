e8ed4af81e9d002f0bcd7600810ffbd70b6f95ca
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-05-09 04:38:39 -0700

    missing files.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_all - Function
+ *
+ * Description: Prints anything.
+ *
+ * @format: The data type of arguments.
+ *
+ * Return: No return because it's a void function.
+ */
+void print_all(const char * const format, ...)
+{
+	int i;
+
+	i = 0;
+	while (*(format + i) != '\0')
+	{
+		if ()
+		i++;
+	}
+}
