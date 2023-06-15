e50987e2f1c06e4014b86a5db4bda85943e6c88a
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-06-15 12:17:58 -0700

    The largest palindrome made from the product of two 3-digit numbers.

---
+++ 0x17-doubly_linked_lists/is_palin_num.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def is_palin_num(number):
+    num = str(number)
+    i = 0
+    j = len(num) - 1
+    while i < len(num) and j > -1:
+        if num[i] != num[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
+first_num = 0
+sec_num = 0
+prod = 0
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        if is_palin_num(i * j):
+            first_num = i
+            sec_num = j
+            prod = i * j
+print("{} * {} = {}".format(first_num, sec_num, prod))
+
